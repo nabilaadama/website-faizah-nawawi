4d853bf81ec54c5e2d367e70a51f04c79e6ae430
ayuwdyaa <ayuwidyaningrum0406@gmail.com>
2025-05-07 16:08:35 +0800

    add users admin page

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,7 +1,8 @@
 const AdminUsersPage = () => {
   return (
-    <div>
-      <div>Admin Users Page</div>
+    <div className="p-8 text-black">
+        <h1 className="text-3xl font-bold mb-4">Admin Users Page</h1>
+        <p>halooo</p>
     </div>
   );
 };
