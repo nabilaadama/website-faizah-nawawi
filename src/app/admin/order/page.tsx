899a3fa0484290458b92ca6b9ad530ad002c32a9
ayuwdyaa <ayuwidyaningrum0406@gmail.com>
2025-05-07 16:10:00 +0800

    add order admin page

---
+++ src/app/admin/order/page.tsx
@@ -0,0 +1,11 @@
+const OrderPage = () => {
+    return (
+      <div className="p-8 text-black">
+        <h1 className="text-3xl font-bold mb-4">Order Page</h1>
+        <p>alohaaaa</p>
+      </div>
+    );
+  };
+  
+  export default OrderPage;
+  
